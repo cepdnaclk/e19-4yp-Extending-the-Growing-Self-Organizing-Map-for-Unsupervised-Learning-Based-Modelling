17b99292bcbf2f63b0ece01c804de68d88fae758
bimbara28 <115542059+Bimbara28@users.noreply.github.com>
2025-07-30 19:11:55 +0530

    FInal Results

---
+++ code/Objective02/Hierarchcical_codes/GSOM.py
@@ -0,0 +1,396 @@
+import numpy as np
+import pandas as pd
+from scipy.spatial import distance
+import scipy
+from tqdm import tqdm
+import math
+from bigtree import Node, findall, find, tree_to_dot
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib import colors
+import networkx as nx
+import pydot
+
+class GSOM:
+    def __init__(self, spred_factor, dimensions, distance='euclidean', initialize='random', learning_rate=0.3,
+                 smooth_learning_factor=0.8, max_radius=6, FD=0.1, r=3.8, alpha=0.9, initial_node_size=1000):
+        """
+        GSOM structure: keep dictionary to x,y coordinates and numpy array to keep weights
+        :param spred_factor: spread factor of GSOM graph
+        :param dimensions: weight vector dimensions
+        :param distance: distance method: support scipy.spatial.distance.cdist
+        :param initialize: weight vector initialize method
+        :param learning_rate: initial training learning rate of weights
+        :param smooth_learning_factor: smooth learning factor to change the initial smooth learning rate from training
+        :param max_radius: maximum neighbourhood radius
+        :param FD: spread weight value
+        :param r: learning rate update value
+        :param alpha: learning rate update value
+        :param initial_node_size: initial node allocation in memory
+        """
+        self.initial_node_size = initial_node_size
+        self.node_count = 0
+        self.map = {}
+        self.node_list = np.zeros((self.initial_node_size, dimensions))
+        self.node_coordinate = np.zeros((self.initial_node_size, 2))
+        self.node_errors = np.zeros(self.initial_node_size, dtype=np.longdouble)
+        self.spred_factor = spred_factor
+        self.groth_threshold = -dimensions * math.log(self.spred_factor)
+        self.FD = FD
+        self.R = r
+        self.ALPHA = alpha
+        self.dimentions = dimensions
+        self.distance = distance
+        self.initialize = initialize
+        self.learning_rate = learning_rate
+        self.smooth_learning_factor = smooth_learning_factor
+        self.max_radius = max_radius
+        self.node_labels = None
+        self.output = None
+        self.predictive = None
+        self.active = None
+        self.sequence_weights = None
+        self.path_tree = {}
+        self.initialize_GSOM()
+
+    def initialize_GSOM(self):
+        self.path_tree = Node("root", x=0.01, y=0.01, node_number=-1, distance=0)
+        for x, y in [(1, 1), (1, 0), (0, 1), (0, 0)]:
+            self.insert_node_with_weights(x, y)
+
+    def insert_new_node(self, x, y, weights, parent_node=None):
+        if self.node_count >= self.initial_node_size:
+            raise MemoryError("Node size out of bound")
+        self.map[(x, y)] = self.node_count
+        self.node_list[self.node_count] = weights
+        self.node_coordinate[self.node_count][0] = x
+        self.node_coordinate[self.node_count][1] = y
+        
+        distance_from_parent = 0
+        new_node = Node(str(self.node_count), x=x, y=y, node_number=self.node_count, distance=distance_from_parent)
+        if parent_node is not None:
+            if (parent_node.x, parent_node.y) in self.map:
+                distance_from_parent = scipy.spatial.distance.cdist(
+                    weights.reshape(1, -1),
+                    self.node_list[self.map[(parent_node.x, parent_node.y)]].reshape(1, -1),
+                    self.distance
+                )[0][0]
+                new_node.distance = distance_from_parent
+            new_node.parent = parent_node
+        else:
+            raise ValueError("Parent node is not provided")
+        
+        self.node_count += 1
+
+    def insert_node_with_weights(self, x, y):
+        if self.initialize == 'random':
+            node_weights = np.random.rand(self.dimentions)
+        else:
+            raise NotImplementedError("Initialization method not supported")
+        self.insert_new_node(x, y, node_weights, parent_node=self.path_tree)
+
+    def _get_learning_rate(self, prev_learning_rate):
+        return self.ALPHA * (1 - (self.R / self.node_count)) * prev_learning_rate
+
+    def _get_neighbourhood_radius(self, total_iteration, iteration):
+        time_constant = total_iteration / math.log(self.max_radius)
+        return self.max_radius * math.exp(- iteration / time_constant)
+
+    def _new_weights_for_new_node_in_middle(self, winnerx, winnery, next_nodex, next_nodey):
+        weights = (self.node_list[self.map[(winnerx, winnery)]] + self.node_list[
+            self.map[(next_nodex, next_nodey)]]) * 0.5
+        return weights
+
+    def _new_weights_for_new_node_on_one_side(self, winnerx, winnery, next_nodex, next_nodey):
+        weights = (2 * self.node_list[self.map[(winnerx, winnery)]] - self.node_list[
+            self.map[(next_nodex, next_nodey)]])
+        return weights
+
+    def _new_weights_for_new_node_one_older_neighbour(self, winnerx, winnery):
+        weights = np.full(self.dimentions, (max(self.node_list[self.map[(winnerx, winnery)]]) + min(
+            self.node_list[self.map[(winnerx, winnery)]])) / 2)
+        return weights
+
+    def grow_node(self, wx, wy, x, y, side):
+        if not (x, y) in self.map:
+            if side == 0:  # left
+                if (x - 1, y) in self.map:
+                    weights = self._new_weights_for_new_node_in_middle(wx, wy, x - 1, y)
+                elif (wx + 1, wy) in self.map:
+                    weights = self._new_weights_for_new_node_on_one_side(wx, wy, wx + 1, wy)
+                elif (wx, wy + 1) in self.map:
+                    weights = self._new_weights_for_new_node_on_one_side(wx, wy, wx, wy + 1)
+                elif (wx, wy - 1) in self.map:
+                    weights = self._new_weights_for_new_node_on_one_side(wx, wy, wx, wy - 1)
+                else:
+                    weights = self._new_weights_for_new_node_one_older_neighbour(wx, wy)
+            elif side == 1:  # right
+                if (x + 1, y) in self.map:
+                    weights = self._new_weights_for_new_node_in_middle(wx, wy, x + 1, y)
+                elif (wx - 1, wy) in self.map:
+                    weights = self._new_weights_for_new_node_on_one_side(wx, wy, wx - 1, wy)
+                elif (wx, wy + 1) in self.map:
+                    weights = self._new_weights_for_new_node_on_one_side(wx, wy, wx, wy + 1)
+                elif (wx, wy - 1) in self.map:
+                    weights = self._new_weights_for_new_node_on_one_side(wx, wy, wx, wy - 1)
+                else:
+                    weights = self._new_weights_for_new_node_one_older_neighbour(wx, wy)
+            elif side == 2:  # top
+                if (x, y + 1) in self.map:
+                    weights = self._new_weights_for_new_node_in_middle(wx, wy, x, y + 1)
+                elif (wx, wy - 1) in self.map:
+                    weights = self._new_weights_for_new_node_on_one_side(wx, wy, wx, wy - 1)
+                elif (wx + 1, wy) in self.map:
+                    weights = self._new_weights_for_new_node_on_one_side(wx, wy, wx + 1, wy)
+                elif (wx - 1, wy) in self.map:
+                    weights = self._new_weights_for_new_node_on_one_side(wx, wy, wx - 1, wy)
+                else:
+                    weights = self._new_weights_for_new_node_one_older_neighbour(wx, wy)
+            elif side == 3:  # bottom
+                if (x, y - 1) in self.map:
+                    weights = self._new_weights_for_new_node_in_middle(wx, wy, x, y - 1)
+                elif (wx, wy + 1) in self.map:
+                    weights = self._new_weights_for_new_node_on_one_side(wx, wy, wx, wy + 1)
+                elif (wx + 1, wy) in self.map:
+                    weights = self._new_weights_for_new_node_on_one_side(wx, wy, wx + 1, wy)
+                elif (wx - 1, wy) in self.map:
+                    weights = self._new_weights_for_new_node_on_one_side(wx, wy, wx - 1, wy)
+                else:
+                    weights = self._new_weights_for_new_node_one_older_neighbour(wx, wy)
+            else:
+                raise ValueError("Invalid side specified")
+            
+            weights[weights < 0] = 0.0
+            weights[weights > 1] = 1.0
+            
+            parent_node = find(self.path_tree, lambda node: node.x == wx and node.y == wy)
+            self.insert_new_node(x, y, weights, parent_node=parent_node)
+
+    def spread_wights(self, x, y):
+        leftx, lefty = x - 1, y
+        rightx, righty = x + 1, y
+        topx, topy = x, y + 1
+        bottomx, bottomy = x, y - 1
+        self.node_errors[self.map[(x, y)]] = self.groth_threshold / 2
+        if (leftx, lefty) in self.map:
+            self.node_errors[self.map[(leftx, lefty)]] *= (1 + self.FD)
+        if (rightx, righty) in self.map:
+            self.node_errors[self.map[(rightx, righty)]] *= (1 + self.FD)
+        if (topx, topy) in self.map:
+            self.node_errors[self.map[(topx, topy)]] *= (1 + self.FD)
+        if (bottomx, bottomy) in self.map:
+            self.node_errors[self.map[(bottomx, bottomy)]] *= (1 + self.FD)
+
+    def adjust_wights(self, x, y, rmu_index):
+        leftx, lefty = x - 1, y
+        rightx, righty = x + 1, y
+        topx, topy = x, y + 1
+        bottomx, bottomy = x, y - 1
+        if (leftx, lefty) in self.map and (rightx, righty) in self.map and \
+           (topx, topy) in self.map and (bottomx, bottomy) in self.map:
+            self.spread_wights(x, y)
+        else:
+            self.grow_node(x, y, leftx, lefty, 0)
+            self.grow_node(x, y, rightx, righty, 1)
+            self.grow_node(x, y, topx, topy, 2)
+            self.grow_node(x, y, bottomx, bottomy, 3)
+        self.node_errors[rmu_index] = self.groth_threshold / 2
+
+    def winner_identification_and_neighbourhood_update(self, data_index, data, radius, learning_rate):
+        out = scipy.spatial.distance.cdist(self.node_list[:self.node_count], data[data_index, :].reshape(1, self.dimentions), self.distance)
+        rmu_index = out.argmin()
+        error_val = out.min()
+        rmu_x = int(self.node_coordinate[rmu_index][0])
+        rmu_y = int(self.node_coordinate[rmu_index][1])
+
+        error = data[data_index] - self.node_list[rmu_index]
+        self.node_list[self.map[(rmu_x, rmu_y)]] += learning_rate * error
+
+        mask_size = round(radius)
+        for i in range(rmu_x - mask_size, rmu_x + mask_size):
+            for j in range(rmu_y - mask_size, rmu_y + mask_size):
+                if (i, j) in self.map and (i != rmu_x or j != rmu_y):
+                    error = self.node_list[rmu_index] - self.node_list[self.map[(i, j)]]
+                    distance = (rmu_x - i)**2 + (rmu_y - j)**2
+                    eDistance = np.exp(-1.0 * distance / (2.0 * (radius**2)))
+                    self.node_list[self.map[(i, j)]] += learning_rate * eDistance * error
+        return rmu_index, rmu_x, rmu_y, error_val
+
+    def smooth(self, data, radius, learning_rate):
+        for data_index in range(data.shape[0]):
+            self.winner_identification_and_neighbourhood_update(data_index, data, radius, learning_rate)
+
+    def grow(self, data, radius, learning_rate):
+        for data_index in range(data.shape[0]):
+            rmu_index, rmu_x, rmu_y, error_val = self.winner_identification_and_neighbourhood_update(
+                data_index, data, radius, learning_rate)
+            self.node_errors[rmu_index] += error_val
+            if self.node_errors[rmu_index] > self.groth_threshold:
+                self.adjust_wights(rmu_x, rmu_y, rmu_index)
+
+    def fit(self, data, training_iterations, smooth_iterations):
+        current_learning_rate = self.learning_rate
+        for i in tqdm(range(training_iterations), desc="Growing"):
+            radius_exp = self._get_neighbourhood_radius(training_iterations, i)
+            if i != 0:
+                current_learning_rate = self._get_learning_rate(current_learning_rate)
+            self.grow(data, radius_exp, current_learning_rate)
+
+        current_learning_rate = self.learning_rate * self.smooth_learning_factor
+        for i in tqdm(range(smooth_iterations), desc="Smoothing"):
+            radius_exp = self._get_neighbourhood_radius(training_iterations, i)
+            if i != 0:
+                current_learning_rate = self._get_learning_rate(current_learning_rate)
+            self.smooth(data, radius_exp, current_learning_rate)
+        out = scipy.spatial.distance.cdist(self.node_list[:self.node_count], data, self.distance)
+        return out.argmin(axis=0)
+
+    def predict(self, data, index_col, label_col=None):
+        import numpy as np
+        import pandas as pd
+        import scipy.spatial
+
+    # === Select feature columns ===
+        weight_columns = list(data.columns.values)
+        output_columns = [index_col]
+
+        if label_col:
+            weight_columns.remove(label_col)
+            output_columns.append(label_col)
+        weight_columns.remove(index_col)
+
+    # === Extract features and metadata ===
+        data_n = data[weight_columns].to_numpy()
+        data_out = pd.DataFrame(data[output_columns])
+
+    # === Convert node_list to 2D matrix ===
+        try:
+            node_matrix = np.stack([np.asarray(n).flatten() for n in self.node_list[:self.node_count]])
+        except Exception as e:
+            print("❌ Failed to stack node_list. Check node dimensions.")
+            raise e
+
+    # === Final shape check ===
+        if node_matrix.shape[1] != data_n.shape[1]:
+            raise ValueError(
+                f"❌ Shape mismatch: node dim = {node_matrix.shape[1]}, input dim = {data_n.shape[1]}"
+         )
+
+    # === Calculate distances and BMU assignment ===
+        out = scipy.spatial.distance.cdist(node_matrix, data_n, self.distance)
+        data_out["output"] = out.argmin(axis=0)
+
+    # === Group output ===
+        grp_output = data_out.groupby("output")
+        dn = grp_output[index_col].apply(list).reset_index().set_index("output")
+        if label_col:
+            dn[label_col] = grp_output[label_col].apply(list)
+
+        dn = dn.reset_index()
+        dn["hit_count"] = dn[index_col].apply(lambda x: len(x))
+        dn["x"] = dn["output"].apply(lambda x: self.node_coordinate[x, 0])
+        dn["y"] = dn["output"].apply(lambda x: self.node_coordinate[x, 1])
+
+        self.node_labels = dn
+        self.output = data_out
+        return self.node_labels
+
+
+    def get_paths(self):
+        paths = []
+        for leaf in self.path_tree.leaves:
+            path = []
+            current = leaf
+            while current is not None:
+                path.append(current)
+                current = current.parent
+            path.reverse()
+            paths.append(path)
+        return paths
+
+    def build_skeleton(self, data):
+        hit_points = []
+        out = scipy.spatial.distance.cdist(self.node_list[:self.node_count], data, self.distance)
+        winner_indices = out.argmin(axis=0)
+        hit_points = np.unique(winner_indices).tolist()
+        
+        skeleton_connections = []
+        pos_edges = []
+        for path in self.get_paths():
+            for i in range(1, len(path) - 1):
+                parent = path[i]
+                child = path[i + 1]
+                parent_idx = parent.node_number
+                child_idx = child.node_number
+                if parent_idx >= 0 and child_idx >= 0:
+                    skeleton_connections.append((parent_idx, child_idx))
+                    pos_edges.append((parent_idx, child_idx))
+        
+        pos_nodes = set()
+        for _, j in skeleton_connections:
+            pos_nodes.add(j)
+        for i in hit_points:
+            if i not in pos_nodes:
+                min_dist = float('inf')
+                nearest = None
+                for j in pos_nodes:
+                    dist = scipy.spatial.distance.cdist(
+                        self.node_list[i].reshape(1, -1),
+                        self.node_list[j].reshape(1, -1),
+                        self.distance
+                    )[0][0]
+                    if dist < min_dist:
+                        min_dist = dist
+                        nearest = j
+                if nearest is not None:
+                    skeleton_connections.append((i, nearest))
+                    parent_node = find(self.path_tree, lambda node: node.node_number == nearest)
+                    if parent_node:
+                        Node(str(i), x=self.node_coordinate[i, 0], y=self.node_coordinate[i, 1],
+                             node_number=i, distance=min_dist, parent=parent_node)
+        
+        junctions = []
+        for i, j in skeleton_connections:
+            if i not in hit_points and j in hit_points:
+                junctions.append(i)
+            elif j not in hit_points and i in hit_points:
+                junctions.append(j)
+        
+        return hit_points, skeleton_connections, junctions, pos_edges
+
+    def separate_clusters(self, data, max_clusters=4):
+        hit_points, skeleton_connections, junctions, pos_edges = self.build_skeleton(data)
+        segments = []
+        for i, j in skeleton_connections:
+            if (i in hit_points or i in junctions) and (j in hit_points or j in junctions):
+                dist = scipy.spatial.distance.cdist(
+                    self.node_list[i].reshape(1, -1),
+                    self.node_list[j].reshape(1, -1),
+                    self.distance
+                )[0][0]
+                segments.append((i, j, dist))
+        
+        segments.sort(key=lambda x: x[2], reverse=True)
+        
+        G = nx.Graph(skeleton_connections)
+        clusters = []
+        remaining_connections = skeleton_connections.copy()
+        # for i, j, dist in segments:
+        #     if (i, j) in remaining_connections:
+        #         remaining_connections.remove((i, j))
+        #     G.remove_edge(i, j)
+        #     clusters.append(list(nx.connected_components(G)))
+        #     print(f"Removed segment {i}-{j}, Distance: {dist}")
+        #     if len(clusters[-1]) >= max_clusters:
+        #         break
+        for i, j, dist in segments:
+            if (i, j) in remaining_connections:
+                remaining_connections.remove((i, j))
+            # Only remove the edge if it exists
+            if G.has_edge(i, j):
+                G.remove_edge(i, j)
+                clusters.append(list(nx.connected_components(G)))
+                print(f"Removed segment {i}-{j}, Distance: {dist}")
+                if len(clusters[-1]) >= max_clusters:
+                    break
+        return clusters, segments, remaining_connections, pos_edges
